a35709c8b887e78a4560cc62d01871fed9ceef86
mpektkd <mpektkd@yahoo.com>
2021-07-26 01:57:59 +0300

    ormconfig change

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,11 @@ import { ConnectionOptions } from 'typeorm';
 
 const connectionOptions: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'ec2-54-220-195-236.eu-west-1.compute.amazonaws.com',
   port: 5432,
-  username: 'postgres',
-  password: 'mpekristkd68',
-  database: 'ntua',
+  username: 'vtzrffjbqxcrmn',
+  password: 'cc967ef9547db0b4febd78f33a581f2c122728495c536df1ebccceb19a5d7a4d',
+  database: 'd9hn101fd53u27',
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migration/*{.ts,.js}'],
   cli: {
