8c680c74ecad8fcb56835c7c4a59648caa9d17e5
mpektkd <mpektkd@yahoo.com>
2021-07-26 22:10:44 +0300

    SSL on, 1st try(maybe last)

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,11 +1,18 @@
 "use strict";
 const connectionOptions = {
+    name: 'deafult',
     type: 'postgres',
     host: 'ec2-54-220-195-236.eu-west-1.compute.amazonaws.com',
     port: 5432,
     username: 'vtzrffjbqxcrmn',
     password: 'cc967ef9547db0b4febd78f33a581f2c122728495c536df1ebccceb19a5d7a4d',
     database: 'd9hn101fd53u27',
+    "ssl": true,
+    "extra": {
+        "ssl": {
+            "rejectUnauthorized": false
+        }
+    }
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/migration/*{.ts,.js}'],
     cli: {
