abbf0bfcbcc0149afc32fe862085bd41b7c46315
mpektkd <mpektkd@yahoo.com>
2021-07-26 22:43:17 +0300

    Some details have been corrected

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,12 +1,11 @@
 "use strict";
 const connectionOptions = {
-    name: 'deafult',
     type: 'postgres',
-    host: 'ec2-54-220-195-236.eu-west-1.compute.amazonaws.com',
+    host: 'localhost',
     port: 5432,
-    username: 'vtzrffjbqxcrmn',
-    password: 'cc967ef9547db0b4febd78f33a581f2c122728495c536df1ebccceb19a5d7a4d',
-    database: 'd9hn101fd53u27',
+    username: 'postgres',
+    password: 'mpekristkd68',
+    database: 'ntua',
     entities: ['dist/**/*.entity{.ts,.js}'],
     migrations: ['dist/migration/*{.ts,.js}'],
     cli: {
